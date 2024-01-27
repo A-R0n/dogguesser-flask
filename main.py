d93d690416be79f28de280cb01b65d8b55973769
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2024-01-27 15:54:12 -0600

    video capture with flask app thru local host with animated camera button overlay

--- main.py
+++ main.py
@@ -7,6 +7,34 @@ from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 import torch
 import sys
+from flask import Flask, render_template, Response
+
+FLASK_PORT = 80
+## if port 80 doesnt work
+# FLASK_PORT = 5001
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen_frames():
+    camera = cv2.VideoCapture(0)  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
 
 def main():
     Main()
@@ -155,4 +183,6 @@ class Main:
 
 
 if __name__ == '__main__':
-    main()
+    # app.run(host="0.0.0.0", port=FLASK_PORT)
+    app.run(debug=True)
+    # main()
